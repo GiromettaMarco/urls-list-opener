f2da1d0638332030de94ee5f02f42ef0fded5adc
Marco Girometta <girometta.marco@gmail.com>
2021-12-01 14:11:34 +0100

    Modularity and Docs - part 2
    - Changed JS to jQuery plugin (uloOpen)
    - Updated documentation
    - Added screenshot

--- README.md
+++ README.md
@@ -6,16 +6,31 @@ This is a little application to open a list o URLs and extract data from its con
 
 - Write your URLs inside `list.txt` separated by a new line.
 - Open `single.php` and edit "Data delimiters" variables to find what you are looking for.
-- (optional) Open `index.html` and edit `opener` properties in JS (see below).
+- (optional) Open `index.html` and add options to `.uloOpen()` in JS (see below).
 
 ### JS options
 
 | Property   | Type    | Default        | Description                                                  | Example                     |
 | ---------- | ------- | -------------- | ------------------------------------------------------------ | --------------------------- |
-| `listPath` | string  | `'list.txt'`   | Path to the file containing the list                         | `'my_folder/my_list.text'`  |
+| `listPath` | string  | `'list.txt'`   | Path to the file containing the list                         | `'my_folder/my_list.txt'`  |
 | `engine`   | string  | `'single.php'` | Path to the PHP file for reading and extract data            | `'my_folder/my_engine.php'` |
 | `delay`    | integer | `2000`         | Delay time in milliseconds between every request. Useful to avoid being detected as spam |                             |
 
 ``````javascript
-opener.listPath = 'my_list.txt';
+$( "#ulo-table-body" ).uloOpen({
+    listPath: 'my_list.txt'
+});
 ``````
+
+### JS Layout options
+You can choose where to print the application output:
+``````javascript
+$( ".my-table tbody" ).uloOpen({
+    statusSelector:   '.my-status',
+    currentSelector:  '.my-current',
+    totalSelector:    '.my-total'
+});
+``````
+
+## Screen Shots
+![List Filled](screenshot.jpg)
---
+++ js/ulo.js
@@ -0,0 +1,121 @@
+(function( $ ) {
+
+  var settings = {};
+  var list = [];
+  var count = 0;
+
+  $.fn.uloOpen = function( options ) {
+
+    settings = $.extend( {}, $.fn.uloOpen.defaults, options );
+    settings.tableBody = this;
+
+    $.fn.uloOpen.run();
+
+    return this;
+
+  };
+
+  $.fn.uloOpen.defaults = {
+    // Functional options
+    listPath: 'list.txt',
+    engine:   'single.php',
+    delay:    2000,
+    // Layout related options
+    statusSelector:  '#ulo-status',
+    currentSelector:  '#ulo-current',
+    totalSelector:    '#ulo-total'
+  };
+
+  $.fn.uloOpen.run = function() {
+    $.ajax({
+      url: 'list.php?fl=' + encodeURIComponent( settings.listPath ),
+      success: function( result ) {
+        list = result.list;
+        $( settings.statusSelector ).addClass( 'ulo-active' );
+        $( settings.totalSelector ).html( list.length );
+      },
+      error: function( jqXHR, textStatus, errorThrown ) {
+        console.log( jqXHR );
+        console.log( textStatus );
+        console.log( errorThrown );
+      },
+      complete: function( jqXHR, textStatus ) {
+        if ( textStatus === 'success' ) {
+          $.fn.uloOpen.fillTable();
+        }
+      }
+    });
+  };
+
+  $.fn.uloOpen.fillTable = function() {
+    $.each( list, function( index, value ) {
+      setTimeout( function() {
+        $.fn.uloOpen.writeRow( index, value );
+        if ( index === (list.length - 1) ) {
+          $( settings.statusSelector ).removeClass( 'ulo-active' );
+          $( settings.statusSelector ).addClass( 'ulo-done' );
+        }
+      }, settings.delay * index );
+    } );
+  };
+
+  $.fn.uloOpen.writeRow = function( index, value ) {
+    var newID = 'ulo-row-' + index;
+    // Row
+    var newRow = $( '<tr/>', {
+      id: newID
+    } ).appendTo( settings.tableBody );
+    // Column Number
+    $( '<td/>', {
+      class: 'ulo-col-numb',
+      text: index + 1
+    } ).appendTo( newRow );
+    // Column Source
+    var newColumnSource = $( '<td/>', {
+      class: 'ulo-col-link'
+    } ).appendTo( newRow );
+    // Link
+    $( '<a/>', {
+      href: window.location.href + 'single.php?prt=1&url=' + value,
+      target: '_blank',
+      text: 'source'
+    } ).appendTo( newColumnSource );
+    // Column Title
+    var newColumnTitle = $( '<td/>', {
+      class: 'ulo-col-title',
+      text: 'loading ...'
+    } ).appendTo( newRow );
+    // Column Data
+    var newColumnData = $( '<td/>', {
+      class: 'ulo-col-data',
+      text: 'loading ...'
+    } ).appendTo( newRow );
+
+    $.ajax({
+      url: settings.engine,
+      data: {
+        url: value
+      },
+      success: function( result ) {
+        if ( result.status ) {
+          $( newColumnTitle ).html( result.title );
+          $( newColumnData ).html( result.data );
+        } else {
+          $( newColumnTitle ).html( 'invalid url' );
+          $( newColumnData ).html( ':(' );
+        }
+      },
+      error: function( jqXHR, textStatus, errorThrown ) {
+        console.log( jqXHR );
+        console.log( textStatus );
+        console.log( errorThrown );
+        $( newColumnTitle ).html( 'connection error' );
+        $( newColumnData ).html( 'X(' );
+      }
+    });
+
+    count += 1;
+    $( settings.currentSelector ).html( count );
+  };
+
+})(jQuery);
